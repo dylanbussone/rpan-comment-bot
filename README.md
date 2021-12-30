0e8362248c1d2b6daee9b13c62c09908dab9df18
Dylan Bussone <dylanbussone@gmail.com>
2021-12-30 13:36:02 -0800

    Create initial bot

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # rpan-comment-bot
-Reddit bot to find your newest submission and add comments to it
+Reddit bot to find your newest submission and add comments to it.
+
+## Usage
+First you will need to create the following environment variables for your Reddit OAuth2 credentials:
+```
+REDDIT_BOT_USER_AGENT
+REDDIT_BOT_CLIENT_ID
+REDDIT_BOT_CLIENT_SECRET
+REDDIT_BOT_USERNAME
+REDDIT_BOT_PASSWORD
+```
+
+Then, edit the messages list and timeout duration with your desired messages and time between them. The messages will be posted in sequential order on loop.
+
+Run with `python3 main.py`
---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import sys
+import time
+import os
+import praw
+
+messages = [
+    'You should totally follow this guy on [Spotify](https://open.spotify.com/artist/1Op9vSnBgavICOjzdFpM3X) 💃',
+    'Whoa! I\'m on [youtube](https://www.youtube.com/channel/UCkkAYSOwxBFpTaQc4_Y7WnA)',
+    'Is [soundcloud](https://soundcloud.com/dylanbussone) still cool?',
+    '🏄 Radical! Free downloads on [bandcamp](https://dylonious.bandcamp.com/)',
+]
+timeout = 3 * 60
+
+reddit = praw.Reddit(
+    user_agent=os.environ['REDDIT_BOT_USER_AGENT'],
+    client_id=os.environ['REDDIT_BOT_CLIENT_ID'],
+    client_secret=os.environ['REDDIT_BOT_CLIENT_SECRET'],
+    username=os.environ['REDDIT_BOT_USERNAME'],
+    password=os.environ['REDDIT_BOT_PASSWORD'],
+)
+
+new_posts = reddit.redditor("dylonious").submissions.new()
+for post in new_posts:
+    if not post.pinned:
+        print(f'Found post: {post.title}\n{post.url}\n')
+        message_index = 0
+        while True:
+            print('Posting message...')
+            post.reply(messages[message_index % len(messages)])
+            message_index += 1
+            time.sleep(timeout)
